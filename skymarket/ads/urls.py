1ba401ee8069216a37a417d3e361961c881a7137
ksun <ksusha1354@gmail.com>
2023-10-13 23:11:51 +0300

    Fixed with urls

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -2,7 +2,17 @@ from django.urls import include, path
 
 # TODO настройка роутов для модели
 
+from rest_framework_nested import routers
+
+from skymarket.ads.views import CommentViewSet, AdViewSet
+
+ad_router = routers.SimpleRouter()
+ad_router.register("ads", AdViewSet, basename="ads")
+
+comment_router = routers.NestedSimpleRouter(ad_router, "ads", lookup="ad")
+comment_router.register("comments", CommentViewSet, basename="comments")
 
 urlpatterns = [
-
+    path("", include(ad_router.urls)),
+    path("", include(comment_router.urls)),
 ]
--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("redoc-tasks/", include("redoc.urls")),
     path('', include('users.urls')),
-    #path('', include('ads.urls')),
+    path('', include('ads.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
